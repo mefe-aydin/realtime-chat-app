c0ef0be2f3ae545f7e8f2aa5ec74a9d81183e2ac
mefe-aydin <m.efeaydin@outlook.com>
2022-12-26 23:46:24 +0300

    google session authentication added to project

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next';
+import { signIn, signOut, useSession } from 'next-auth/react';
+
+const Home: NextPage = () => {
+  const { data } = useSession();
+  console.log('data', data);
+
+  return (
+    <div>
+      {data?.user ? (
+        <button onClick={() => signOut()}>Sign Out</button>
+      ) : (
+        <button onClick={() => signIn('google')}>Sign In</button>
+      )}
+      {data?.user?.name}
+    </div>
+  );
+};
+
+export default Home;
