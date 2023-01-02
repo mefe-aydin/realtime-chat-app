d28b5f59e96856c4ea3e1bcb25a02a978eeb6e97
mefe-aydin <m.efeaydin@outlook.com>
2023-01-02 23:48:14 +0300

    graphql schema created to backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,15 +3,23 @@ import {
   ApolloServerPluginDrainHttpServer,
   ApolloServerPluginLandingPageLocalDefault,
 } from 'apollo-server-core';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 import express from 'express';
 import http from 'http';
+import typeDefs from './graphql/typeDefs';
+import resolvers from './graphql/resolvers';
 
-async function main(typeDefs, resolvers) {
+async function main() {
   const app = express();
   const httpServer = http.createServer(app);
-  const server = new ApolloServer({
+
+  const schema = makeExecutableSchema({
     typeDefs,
     resolvers,
+  });
+
+  const server = new ApolloServer({
+    schema,
     csrfPrevention: true,
     cache: 'bounded',
     plugins: [
